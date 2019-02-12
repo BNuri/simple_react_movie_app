b6c09d21f691b4b9df3893a83d42ad1d93726439
bnr <luckmon05@naver.com>
2019-02-12 13:33:12 +0900

    Updates

---
+++ precache-manifest.b5aecacacf600ba0a072def6e7d12d09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afa1e7534f3f76bfde82",
+    "url": "/simple_react_movie_app/static/js/runtime~main.afa1e753.js"
+  },
+  {
+    "revision": "ed402959c3cf3dec582e",
+    "url": "/simple_react_movie_app/static/js/main.ed402959.chunk.js"
+  },
+  {
+    "revision": "5b2c6dc19870e2997f52",
+    "url": "/simple_react_movie_app/static/js/1.5b2c6dc1.chunk.js"
+  },
+  {
+    "revision": "ed402959c3cf3dec582e",
+    "url": "/simple_react_movie_app/static/css/main.2b17625b.chunk.css"
+  },
+  {
+    "revision": "de529c6c5f08be778c40208fecad0755",
+    "url": "/simple_react_movie_app/index.html"
+  }
+];
