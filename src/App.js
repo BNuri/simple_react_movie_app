477bdb2711404b5338d0ea2ec1a48234dfddf480
bnr <luckmon05@naver.com>
2019-02-12 12:15:05 +0900

    add CSS

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ class App extends Component {
 
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
     })
     return movies;
   }
@@ -32,9 +38,10 @@ class App extends Component {
   }
 
   render() {
+    const {movies} = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}
+      <div className={movies ? 'App' : 'App--loadin'}>
+        {movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
